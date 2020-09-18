487697ca2232638d94f49dcee9d127f97a4e4430
Jim Menard <jim.menard@warbyparker.com>
2020-09-18 16:36:27 -0400

    print PortMidi input and output names

--- portmidi_test/src/pmtest.go
+++ portmidi_test/src/pmtest.go
@@ -7,7 +7,26 @@ import (
 
 func main() {
 	portmidi.Initialize()
+
 	num_devices := portmidi.CountDevices()
-	fmt.Println("num devices:", num_devices)
+	devices := make([]*portmidi.DeviceInfo, num_devices)
+	for i := 0; i < num_devices; i++ {
+		devices[i] = portmidi.Info(portmidi.DeviceID(i))
+	}
+
+	fmt.Printf("Inputs:\n")
+	for i := 0; i < num_devices; i++ {
+		if devices[i].IsInputAvailable {
+			fmt.Printf("  %s\n", devices[i].Name)
+		}
+	}
+
+	fmt.Printf("Outputs:\n")
+	for i := 0; i < num_devices; i++ {
+		if devices[i].IsOutputAvailable {
+			fmt.Printf("  %s\n", devices[i].Name)
+		}
+	}
+
 	portmidi.Terminate()
 }
